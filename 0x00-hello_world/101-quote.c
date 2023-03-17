04a2a16504ed4c792d722e67400c1bae25c27cf3
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-17 01:15:02 +0000

    committing to C low level langauge

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ *Return: 1 if success.
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
 stdout);
 return (1);
 }
