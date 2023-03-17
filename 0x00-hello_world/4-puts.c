f7cd553f992d4de80e7cfef79fe8b65a1b22df80
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-17 00:01:12 +0000

    committing to C low level langauge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - A program that prints with puts function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
