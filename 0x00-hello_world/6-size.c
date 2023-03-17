f3273e7deadc19f50e85ba1b4c8b397445573fb0
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-17 01:39:13 +0000

    committing to C low level langauge

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-printf("Size of Int: %2d byte(s) \n", sizeof(short int));
-printf("Size of Long Int: %2d byte(s) \n", sizeof(long int));
-printf("Size of Float: %2d byte(s) \n", sizeof(float));
-printf("Size of Double: %2d byte(s) \n", sizeof(double));
-printf("Size of Long Double: %2d byte(s) \n",
-sizeof(long double));
 printf("Size of Char: %2d byte(s) \n", sizeof(char));
+printf("Size of Int: %2d byte(si) \n", sizeof(short int));
+printf("Size of Long Int: %2d byte(s) \n", sizeof(long int));
+printf("Size of long long int: %2d byte(s) \n", sizeof(double));
+printf("Size of Float: %2d byte(s) \n", sizeof(float));
 return (0);
 }
