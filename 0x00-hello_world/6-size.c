4e98bd154995db9793c4b24a1bf76959af1f2f4d
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-17 01:33:36 +0000

    committing to C low level langauge

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-int integerType;
-float floatType;
-double doubleType;
-char charType;
-
-printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
-printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
-printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
-printf("Size of Double Data Types in C = %2d bytes \n", sizeof(double));
-printf("Size of Long Double Data Types in C = %2d bytes \n",
+printf("Size of Int: %2d byte(s) \n", sizeof(short int));
+printf("Size of Long Int: %2d byte(s) \n", sizeof(long int));
+printf("Size of Float: %2d byte(s) \n", sizeof(float));
+printf("Size of Double: %2d byte(s) \n", sizeof(double));
+printf("Size of Long Double: %2d byte(s) \n",
 sizeof(long double));
-printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
+printf("Size of Char: %2d byte(s) \n", sizeof(char));
 return (0);
 }
