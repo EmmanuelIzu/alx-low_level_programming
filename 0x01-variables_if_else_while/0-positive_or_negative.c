00857f07860d26985434dc8347b39da5af4a5226
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-18 05:59:24 +0000

    commiting the c program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,21 +8,21 @@ int main(void)
 /*checks for positive negative and zero number */
 {
 	int n;
-	/*this selects a randon number */
+/*this selects a randon number */
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
-	/*checks if n is greater than zero */
+/*checks if n is greater than zero */
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
 	}
-	/*checks if n is equal to zero */
+/*checks if n is equal to zero */
 	else if (n == 0)
 	{
 	printf("%d is zero\n", n);
 	}
-	/*checks if n is less than zero */
+/*checks if n is less than zero */
 	else
 	{
 	printf("%d is negative\n", n);
