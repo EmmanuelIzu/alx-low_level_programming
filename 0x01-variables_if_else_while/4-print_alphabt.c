7affe3101ea117810c2ef3ac05e3a53568d9777d
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-18 08:03:57 +0000

    commiting the c program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+/*prints the alphabets A to Z in lowercase*/
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+	if (c != 'q' || c != 'e')
+	{
+	putchar(c);
+	}
+	++c;
+	}
+	putchar('\n');
+
+return (0);
+}
