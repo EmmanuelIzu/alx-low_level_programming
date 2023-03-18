84cd8ab75725232db27f2457c1ab918609e67852
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-18 09:56:40 +0000

    commit low level c programs

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+/*prints all the numbers in base sixteen*/
+{
+
+	
+return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+/*prints the alphabets A to Z in lowercase*/
+{
+	char c;
+
+	c = 0;
+
+	while (c < 10)
+	{
+	putchar('0'+c);
+	putchar(',');
+	putchar(' ');
+	++c;
+	}
+	putchar('\n');
+
+return (0);
+}
