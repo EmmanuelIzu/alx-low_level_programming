0171c78f5e65e75e71c60ebea36461ce7e1674a9
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-18 08:19:37 +0000

    commiting the c program

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+/*prints the alphabets A to Z in lowercase*/
+{
+	int c;
+
+	c = 0;
+
+	while (c < 10)
+	{
+	putchar(c);
+	++c;
+	}
+	putchar('\n');
+
+return (0);
+}
