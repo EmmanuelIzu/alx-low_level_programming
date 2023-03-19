b4d6f32c0f238bc8708519bccac5cf5a90e7dc3f
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-19 19:56:00 +0000

    committing c programs

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,35 +16,34 @@ int main(void)
 	while (a <= 9)
 	{
 	d++;
-	b=d;
-	/*printf("%d",b);*/
+	b = d;
 	f++;
-	e=f;
-	while (b <= 9){
-		e++;
-		c=e;
-		while (c <= 9)
-		{
-			putchar('0' + a);
-			putchar('0' + b);
-			putchar('0' + c);
-			if (a == 7 && b == 8 && c == 9){
-                        
-			}else
-			{
-			putchar(',');
-			putchar(' ');
-	
-		}
-			c++; 
-				}
-		c=0;
-		b++;
-		}
-	b=0;
+	e = f;
+	while (b <= 9)
+	{
+	e++;
+	c = e;
+	while (c <= 9)
+	{
+	putchar('0' + a);
+	putchar('0' + b);
+	putchar('0' + c);
+	if (a == 7 && b == 8 && c == 9)
+	{
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	c++;
+	}
+	c = 0;
+	b++;
+	}
+	b = 0;
 	a++;
-	e=0;
-
+	e = 0;
 	}
 	putchar('\n');
 return (0);
