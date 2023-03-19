a3ea580b434410e9cb1af47155d2a4fcd25cfe78
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-19 11:10:52 +0000

    committing c programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,23 +3,23 @@
 int main(void)
 /*prints the alphabets A to Z in lowercase*/
 {
-	char c;
+	int c;
 
-	c = '0';
+	c = 0;
 
-	while (c <= '9')
+	while (c <= 9)
 	{
-	if (c != '9')
+	if (c != 9)
 	{
-	putchar(c);
+	putchar('0'+c);
 	putchar(',');
 	putchar(' ');
 	}else
 	{
-	putchar(c);
+	putchar('0'+c);
 	}
 	++c;
 	}
 
-return ('\n');
+return (0);
 }
