f7540e0dca594f7908580767f193bbef3fec0f14
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-19 10:30:00 +0000

    committing c programming

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,29 @@
 #include<stdio.h>
 
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+
+
 int main(void)
-/*prints all the numbers in base sixteen*/
 {
 	char base16;
 	char numbers;
 
-	for(numbers = '0'; numbers <= '9'; numbers++){
+	for (numbers = '0'; numbers <= '9'; numbers++)
+	{
 	putchar(numbers);
 	}
 
-	for(base16 = 'a'; base16 <= 'f'; base16++)
-	{	
+	for (base16 = 'a'; base16 <= 'f'; base16++)
+	{
 	putchar(base16);
 	}
-	putchar('\n');	
+
+	putchar('\n');
+
 return (0);
 }
