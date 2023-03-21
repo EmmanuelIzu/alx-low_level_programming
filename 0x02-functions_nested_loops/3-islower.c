d7f967c520d5a58ea8fd31194f8e929cc5cfd42a
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-21 23:35:23 +0000

    committing nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include<ctype.h>
+/**
+ * _islower - checks if the input is a lower character
+ * return: return the truth parameter
+ */
+
+
+int _islower(int c){
+
+	int ret = islower(c);
+return(ret);	
+}
