ccc7ce6301236cb1345d90af43a1cc4664fe767e
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-21 22:55:33 +0000

    committing nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print the alphabeth 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char number;
+	char alpha;
+
+	for (number = 0; number <= 9; ++number)
+	{
+
+		{
+		_putchar(alpha2);
+		}
+
+	_putchar('\n');
+	}
+}
