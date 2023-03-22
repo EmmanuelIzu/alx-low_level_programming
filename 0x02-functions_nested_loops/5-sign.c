06e359c75e0f4a883f29a4b0621177ec82b8bb9e
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-22 10:25:42 +0000

    committing nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - function that checks if the value is a negative number
+ * Return: outputs -1 if less than 0, 1 if greater or equal to 0, and 0 if 0
+ * @n: accepts the input for checking
+ * _putchar: prints an output
+ */
+
+
+
+
+int print_sign(int n)
+{
+
+	int check_for_negative = n;
+
+	if (check_for_negative < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else if (check_for_negative >= 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
+	}
+
+}
