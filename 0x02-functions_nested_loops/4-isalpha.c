d17e0c876c35b6a11015179695dc3dc689076519
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-22 07:22:08 +0000

    committing nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+/**
+ * _isalpha - the function that checks for alphabetical letters
+ * Description: accepts and Input and checks if the input is and alphabet
+ * @c: hold the value to be checked
+ * Return: outputs 1 if input is an alphabeth and 0 is anything else
+ */
+
+
+int _isalpha(int c)
+{
+
+	return (isalpha(c));
+}
