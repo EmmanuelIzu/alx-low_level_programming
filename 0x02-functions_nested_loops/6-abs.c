33eabd35337b6ba307d67fa417ff1c76e0bc2b4a
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-22 15:23:27 +0000

    commiting C programming

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * _abs - accepts the input to be computed
+ * Return: return the absolute value to the calling function
+ * @a: holds the input before computation
+ * Description: computes the absolute value of an input integer
+ */
+
+
+int _abs(int a)
+{
+	int absolute = abs(a);
+
+return (absolute);
+}
