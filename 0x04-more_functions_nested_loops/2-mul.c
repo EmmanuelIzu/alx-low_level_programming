f673ebfee528948fd6415d7b50eb35695b685980
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-23 14:44:09 +0000

    commiting

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+ * mul - a function that checks for upper case
+ * Return: return 1 if true and 0 if false
+ * @a: character that hold first value
+ * @b: character that hold first value
+ */
+
+
+int mul(int a, int b)
+{
+
+	int value_1 = a;
+	int value_2 = b;
+
+	int multiplied_value = value_1 * value_2;
+return (multiplied_value);
+}
