d28289a13b5cb2a55a936804b8274c568c38f079
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-23 15:17:09 +0000

    commiting

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - function that print numbers from 0 to 9
+ * _putchar: prints a character int
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+
+}
