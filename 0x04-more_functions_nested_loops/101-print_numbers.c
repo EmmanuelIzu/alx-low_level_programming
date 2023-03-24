baf4e353632e7e3156a9a8a4f4d323f98eed2c1b
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-24 20:20:04 +0000

    committing the C programs

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_number - function that prints intergers
+ * @n: holding variable
+ */
+
+
+void print_number(int n)
+{
+m = n;
+
+if (n < 0)
+{
+n *= -1;
+m = n;
+_putchar('-');
+
+}
+
+m /= 10;
+if (m != o)
+	print_number(m);
+	_putchar((unsigned int) n % 10 + '0');
+
+
+
+
+
+
+
+
+
+}
--- 0x04-more_functions_nested_loops/testit.c
+++ 0x04-more_functions_nested_loops/testit.c
@@ -1,8 +1,50 @@
-#include "main.h"
+#include <stdio.h>
+/* print_something - function to print something
+*
+* Return: Always 0
+**/
+int print_something(int num);
+void print_shape(int num1, int num2);
+
 
 int main(void){
-
-	int v = _putchar('a');
+	 print_shape(4, 3);
 
 return (0);
 }
+
+
+
+
+int print_something(int num)
+{
+    int i, j;
+    for (i = 0; i < num + 1; i++)
+    {
+        for (j = 0; j < num + 1; j++)
+        {
+            printf("%d", i * j);
+            if (j < num)
+            {
+                printf(", ");
+            }
+        }
+        printf("\n");
+    }
+    return (0);
+   }
+
+
+void print_shape(int num1, int num2)
+{
+    int idx1, idx2;
+    for (idx1 = 0; idx1 < num1; idx1++)
+    {
+        for (idx2 = 0; idx2 < num2; idx2++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
