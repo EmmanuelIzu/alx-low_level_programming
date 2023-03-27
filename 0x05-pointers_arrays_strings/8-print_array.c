adf575795051648e26a47c201df41270c4330885
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-27 23:06:54 +0000

    commit for pointers and arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - the Function that prints arrays
+ * @a: the array pointer
+ * @n: the variable counter
+ */
+void print_array(int *a, int n)
+{
+	int i, neg1;
+
+	neg1 = n - 1;
+
+	for (i = 0; i < n; ++i)
+	{
+
+	if (i == (neg1))
+	{
+	printf("%d", a[neg1]);
+	printf("\n");
+	}
+	else
+	{
+	printf("%d", a[i]);
+	printf(",");
+	printf(" ");
+	}
+
+	}
+
+}
