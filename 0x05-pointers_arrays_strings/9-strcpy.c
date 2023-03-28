898dd108adedbb5c725f2c44d191bc2999b67750
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-28 00:00:24 +0000

    commit for pointers and arrays

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strcpy - The function that copies
+ * @dest: var
+ * @src: var
+ * Return: return the string
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; ++i)
+	{
+	dest[i] = src[i];
+	}
+	dest[++i] = '\0';
+	return (dest);
+}
