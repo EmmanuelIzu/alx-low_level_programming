b0e20b1bcbae35c7f83c62431cb918b55591c799
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-30 11:46:16 +0000

    commit the C _langauge

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - function that prints intergers
+ * @n: holding variable
+ */
+
+
+void print_number(int n)
+{
+	int m = n;
+
+	if (n < 0)
+	{
+	n *= -1;
+	m = n;
+	_putchar('-');
+}
+
+	m /= 10;
+	if (m != 0)
+	print_number(m);
+	_putchar((unsigned int) n % 10 + '0');
+
+
+}
+
