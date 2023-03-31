1592a16d799e8678c3cccb3739951918fdd1609a
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-31 01:51:31 +0000

    committing C prorgams

---
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char *n = "12345678924345743678235745756784776857";
+char *m = "90347906634706972346829145693462596349";
+	char r[100];
+	char r2[10];
+	char *res;
+
+	res = infinite_add(n, m, r, 100);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n = "1234567890";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n = "999999999";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+return(0);
+}
