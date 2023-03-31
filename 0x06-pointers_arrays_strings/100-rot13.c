93b79ac596a4f03378e85f33691ebccb2766dac6
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-03-31 00:04:03 +0000

    commit the C _langauge

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdio.h>
+
+
+char *rot13(char *n)
+{
+char upperside[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char lowerside[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	int i,j;
+	int nlength = strlen(n);
+	int uppersidelength = strlen(upperside);
+
+	for (i = 0; i < nlength; i++)
+	{
+
+		for (j = 0; j < uppersidelength; j++)
+		{
+			if (n[i] == upperside[j])
+		{
+			printf("%c", n[i]);
+			printf("%d", j);
+			printf("%c", lowerside[j]);
+			n[i] = lowerside[j];
+			printf("%c ", n[i]);
+
+			 /*printf("%c ", n[j]);*/
+
+		}
+			/*
+			printf("%c", upperside[i]);
+			printf("%c", lowerside[i]);
+			*/
+			/*printf("%c ", n[j]);*/
+			
+		}
+		
+	}
+return (n);
+}
