b1a052f01482afada293a92562bc71bf852a4cd8
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-03 14:21:21 +0000

    committing low level C language

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include<string.h>
+
+/**
+ * _strspn - Function
+ * @s: constant Variable
+ * @accept: Constant variable
+ * Return: return value
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	strpn(s, accept);
+
+return (s);
+}
