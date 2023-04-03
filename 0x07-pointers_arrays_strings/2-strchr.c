78dd5219653de7f5f5b75c0542d764678d373e9a
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-03 13:44:22 +0000

    committing low level C language

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include<string.h>
+/**
+ * _strchr - Function.
+ * @s: Input variable
+ * @c: Input variable
+ * Return: Returns a pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+
+return (s);
+}
