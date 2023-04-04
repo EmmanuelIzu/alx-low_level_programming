f81d87f0a5f60dd73afc74efc56ebee906b9fcd4
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-04 15:22:02 +0000

    committing C programs

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,13 +1,29 @@
 #include<stdio.h>
-
-
+/**
+ * print_diagsums - Main Function
+ * @a: pointer variable
+ * @size: input variable
+ */
 
 void print_diagsums(int *a, int size)
 {
 
+	int i, j, sum1 = 0, sum2 = 0;
 
-printf("%ld", a);
-printf("%d", size);
+	for (i = 0; i < (size * size); i = i + (size + 1))
+	{
 
+		sum1 = sum1 + a[i];
 
+	}
+
+	for (j = size - 1; j <= ((size * size) - (size - 1)); j = j + (size - 1))
+
+	{
+
+		sum2 = sum2 + a[j];
+
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
