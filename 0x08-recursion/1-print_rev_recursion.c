b75b0a0cdb906ef46be78d30b34f11a8a5f521e3
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-05 21:42:23 +0000

    committing to recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
  * _print_rev_recursion - Main Function
  * @s: Input Variable
@@ -9,11 +10,7 @@
 
 void _print_rev_recursion(char *s)
 {
-	int length = strlen(s), i;
-
-	for (i = (length - 1); i >= 0; i--)
-	{
-	_putchar(s[i]);
-	}
+	
+	putchar(*s);
 
 }
