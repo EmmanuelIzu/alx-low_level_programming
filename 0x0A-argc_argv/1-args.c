d3569f23776cb98371be81150a0307dd7c0862b5
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-09 11:17:05 +0000

    committing argc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main -  The program Starts here
+ * @argc: The number of arguments entered
+ * @argv: an argument array
+ * Return: return 0 always
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+return (0);
+}
