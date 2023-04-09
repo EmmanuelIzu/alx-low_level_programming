570f4eae2fec974dbffaf03f2bc8c77dc695c998
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-09 11:57:56 +0000

    committing argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main -  The program Starts here
+ * @argc: The number of arguments entered
+ * @argv: an argument array
+ * Return: return 0 always
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int count;
+	(void)argc;
+
+	if (argc > 1)
+	{
+	count = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", count);
+	}
+	else
+	{
+	printf("Error");
+	return (1);
+	}
+
+return (0);
+}
