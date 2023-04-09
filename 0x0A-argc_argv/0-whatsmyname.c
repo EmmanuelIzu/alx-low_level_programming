a3e17e605f227f943331f59386778a059dc11ff6
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-09 11:08:20 +0000

    committing argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
+/**
+ * main -  The program Starts here
+ * @argc: The number of arguments entered
+ * @argv: an argument array
+ * Return: return 0 always
+ */
 
-int main(int argc, char* argv[])
+
+int main(int argc, char *argv[])
 {
 
 	(void)argc;
