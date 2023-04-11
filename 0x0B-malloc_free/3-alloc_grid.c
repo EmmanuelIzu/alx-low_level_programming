50144339eb4c82bf4abdadf0916a222388a7206d
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-11 22:22:37 +0000

    malloc_free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - allocate 2D array and initialize to zero
+ * @width: input variable
+ * @height: input variable
+ * Return: return NULL or arr pointer
+ */
+
+
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int *) * height);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; ++i)
+	{
+	arr[i] = malloc(sizeof(int) * width);
+
+	}
+
+	for (i = 0; i < height; ++i)
+	{
+	if (arr[i] == NULL)
+		free(arr[i]);
+
+	}
+
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; ++i)
+	{
+		for (j = 0; j < width; ++j)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+return (arr);
+}
