d052d798ce29b11f2d96bdfd8aac2b3fc48d2452
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-12 21:16:15 +0000

    commiting to malloc_free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatnate argv
+ * @ac: Vector length
+ * @av: Input vector
+ * Return: return NULL of pointer array
+ *
+ */
+char *argstostr(int ac, char **av)
+{
+	int k, length = 0, i, l;
+	unsigned int j;
+	char *arr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (k = 1; k < ac; ++k)
+	{
+	length = length + strlen(av[k]);
+	}
+
+	arr = malloc((sizeof(char) * length) + 1);
+
+	if (arr == NULL)
+		return (NULL);
+
+	l = 0;
+
+	for (i = 1; i < ac; ++i)
+	{
+
+		for (j = 0; j < strlen(av[i]); ++j)
+		{
+		arr[l] = av[i][j];
+		++l;
+		}
+		arr[l] = *"\n";
+		++l;
+	}
+
+return (arr);
+}
