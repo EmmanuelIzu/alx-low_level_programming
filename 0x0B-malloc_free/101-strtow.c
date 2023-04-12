66942b14e2e12ca33b84f1cda4894592b9d340e8
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-12 22:42:37 +0000

    commiting to malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,28 @@
 #include <stdlib.h>
 #include "main.h"
+/**
+ * count - counts number of words
+ * @s: input string
+ *
+ * Return: number of words in a string.
+ */
+int count(char *s)
+{
+	int pos = 0, i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ')
+			pos = 0;
+		else if (pos == 0)
+		{
+			pos = 1;
+			j++;
+		}
+		i++;
+	}
+	return (j);
+}
 
 /**
  * **strtow - function that splits string
