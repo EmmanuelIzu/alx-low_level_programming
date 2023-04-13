4c45f0df1c609e0fbdc7f27587bad413a501c182
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-13 23:06:52 +0000

    commiting to malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,12 +15,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int length, i;
 	char *arr;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
 	length = strlen(s1);
 
 	if (n >= strlen(s2))
