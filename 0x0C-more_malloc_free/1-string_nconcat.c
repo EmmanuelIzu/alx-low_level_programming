d8f3c846475a5518eed1caced9fece6269d3f73a
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-14 00:59:26 +0000

    commiting to malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,40 +13,35 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
 
+	while (s1 && s1[length1])
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	/*
-	while (s1 && s1[len1])
+		length1++;
 
-		len1++;
+	while (s2 && s2[length2])
 
-	while (s2 && s2[len2])
+		length2++;
+	
 
-		len2++;
+	if (n < length2)
 
-	*/
-
-	if (n < len2)
-
-		s = malloc(sizeof(char) * (len1 + n + 1));
+		s = malloc(sizeof(char) * (length1 + n + 1));
 
 	else
 
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (length1 + length2 + 1));
 
 
 
-	if (!s)
+	if (s == NULL)
 
 		return (NULL);
 
 
 
-	while (i < len1)
+	while (i < length1)
 
 	{
 
@@ -58,13 +53,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 
 
-	while (n < len2 && i < (len1 + n))
+	while (n < length2 && i < (length1 + n))
 
 		s[i++] = s2[j++];
 
 
 
-	while (n >= len2 && i < (len1 + len2))
+	while (n >= length2 && i < (length1 + length2))
 
 		s[i++] = s2[j++];
 
