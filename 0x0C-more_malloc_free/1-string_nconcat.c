0600baa28c88f14a3a7bbd0b32a747cbdb2ab871
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-14 00:50:03 +0000

    commiting to malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,42 +11,67 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *s;
 
-	unsigned int length1 = 0, length2 = 0, i = 0, j = 0;
-	char *arr;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (n < j)
-		exit(98);
 
-	while (s1 && s1[length1])		
-		length1++;
-		
-	while (s2 && s2[length2])	
-		length2++;
 
-	if (n >= length2)
-		arr = malloc(sizeof(char) * (length1 + length2 + 1));
+	while (s1 && s1[len1])
+
+		len1++;
+
+	while (s2 && s2[len2])
+
+		len2++;
+
+
+
+	if (n < len2)
+
+		s = malloc(sizeof(char) * (len1 + n + 1));
+
 	else
-		arr = malloc(sizeof(char) * (length1 + n + 1));
 
-	if (arr == NULL)
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+
+
+	if (!s)
+
 		return (NULL);
 
-	for (i = 0; i < (length1); ++i)
-		arr[i] = s1[i];
 
-	if (!(n >= (length2)))
-		for (i = 0; i < n ; ++i)
-		{
-			arr[length1] = s2[i];
-			length1++;
-		}
-	else
-		for (i = 0; i < (length2); ++i)
-		{
-		arr[length1] = s2[i];
-		length1++;
-		}
 
-return (arr);
+	while (i < len1)
+
+	{
+
+		s[i] = s1[i];
+
+		i++;
+
+	}
+
+
+
+	while (n < len2 && i < (len1 + n))
+
+		s[i++] = s2[j++];
+
+
+
+	while (n >= len2 && i < (len1 + len2))
+
+		s[i++] = s2[j++];
+
+
+
+	s[i] = '\0';
+
+
+
+	return (s);
+
 }
+
