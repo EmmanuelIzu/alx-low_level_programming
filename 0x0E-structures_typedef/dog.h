2971ab5444e706fd24480667ada14ade47e362f1
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-18 11:06:39 +0000

    commit structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure name.
+ * @name: struct variable.
+ * @age: struct variable.
+ * @owner: struct variable.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
