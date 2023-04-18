082e07f7783774cda19721fb4cccfb5761c4afe3
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-18 13:16:50 +0000

    commit structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,12 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * typedef struct dog dog_t - creates alias name for dog
+ *
+ */
+typedef struct dog dog_t;
+
 
 #endif
 
