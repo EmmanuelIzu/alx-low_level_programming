d66f3872714d6f49e88132025f81e2c86e4267d0
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-18 21:55:54 +0000

    commit structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* struct dog - a dog's basic info
-* @name: First member
-* @age: Second member
-* @owner: Third member
+* struct dog - for dog
+* @name: dog name
+* @age: Dog age
+* @owner: onwer of Dog
 * Description: Longer description
 */
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
+	struct dog
+	{
+	char *name;
+	float age;
+	char *owner;
+	};
 
-/**
-* dog_t - typedef for struct dog
-*/
-typedef struct dog dog_t;
+	typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
 
 #endif
 
