9f825e2d74c25ab16e8b4ae25bcafddad81c862e
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-20 20:04:37 +0000

    commiting to variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,28 +12,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 	char *string;
-	/**
-	 *
-	 * if (n == 0)
-	 *	return;
-	 */
 
-		va_start(args, n);
+	va_start(args, n);
 
-		for (i = 0; i < n; i++)
-		{
-			string = va_arg(args, char*);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char*);
 
-			if (string == NULL)
-				printf("nil");
-			else
-				printf("%s", string);
+		if (string == NULL)
+			printf("nil");
+		else
+			printf("%s", string);
 
-			if (i != (n - 1) && separator != NULL)
-				printf("%s", separator);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 		}
-		printf("\n");
-		va_end(args);
+	printf("\n");
+	va_end(args);
 
 
 
