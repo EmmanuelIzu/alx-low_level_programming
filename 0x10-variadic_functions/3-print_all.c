64e40ec06020d2556fe090e5efb80e4bb88be938
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-20 17:17:53 +0000

    commiting to variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - prints any input
+ * @format: the list of inputs
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *s = "", *str;
+	va_list args;
+
+	if (format != NULL)
+	{
+		va_start(args, format);
+
+	while (format[i])
+	{
+	switch (format[i])
+	{
+		case 'c':
+			printf("%s%c", s, va_arg(args, int));
+			break;
+		case 'i':
+			printf("%s%d", s, va_arg(args, int));
+			break;
+		case 'f':
+			printf("%s%f", s, va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char*);
+
+			if (str == NULL)
+				str = "nil";
+
+			printf("%s%s", s, str);
+			break;
+	}
+	++i;
+	s = ", ";
+
+	}
+	printf("\n");
+	}
+
+}
