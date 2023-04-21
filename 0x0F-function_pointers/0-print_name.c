dc5f0da3867d55ec12c5432bd1314f23203a8844
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-21 08:48:10 +0000

    committing function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - calls a function using a pointer
+ * @name: string variable
+ * @f: pointer to the function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if(f)
+		f(name);
+
+}
