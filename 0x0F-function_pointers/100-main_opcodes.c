566934705d8e6a62776c6a1b8610a5980f968da7
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-21 15:10:29 +0000

    committing function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The program starts here
+ * @argc: count of array
+ * @argv: array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+	
+	i = 0;
+	
+	while (i < bytes)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	i++;
+	}
+	return (0);
+}
+
