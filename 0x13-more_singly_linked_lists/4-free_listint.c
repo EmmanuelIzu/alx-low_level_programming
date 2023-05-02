1c832d1986697a61c38020377159d82edb1a91b2
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-05-02 06:26:23 +0000

    linked message

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - frees a list
+ * @head: the head pointer
+ *
+ */
+
+
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		while (head)
+		{
+			free(head->next);
+
+		free(head);
+		head = NULL;
+	}
+
+	}
+}
