e59358cddb4b0519d1e374d32415beee0330a052
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-05-02 20:22:17 +0000

    commting list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - replace at index
+ * @head: the head pointer
+ * @idx: the index
+ * @n: data
+ * Return: return pointer or NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+
+	listint_t *new;
+	listint_t *hold;
+
+	hold = *head;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL || *head == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (*head != NULL && count < idx)
+	{
+		if (count == (idx - 1))
+		{
+			new->next = hold->next;
+			hold->next = new;
+			return (new);
+		}
+		else
+			hold = hold->next;
+	count++;
+	}
+
+return (NULL);
+}
