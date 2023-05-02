4f83b39d6279f40b39816591c9b66a2953282a18
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-05-02 20:31:13 +0000

    commting list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,9 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 
 	listint_t *new;
-	listint_t *hold;
-
-	hold = *head;
+	listint_t *hold = *head;
 
 	new = malloc(sizeof(listint_t));
 
