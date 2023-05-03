a2676a46f917036049df1303c7951a33d5b8778d
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-05-03 19:44:23 +0000

    commting list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (hold != NULL && count < idx)
+	while (hold && count < idx)
 	{
 		if (count == (idx - 1))
 		{
