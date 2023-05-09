e14b618b562c73fe1865ea5699effed6c07879bb
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-05-09 00:35:09 +0000

    file handling

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	while (r > 0)
+	do
 	{
 		if (from == -1 || r == -1)
 		{
@@ -94,8 +94,8 @@ int main(int argc, char *argv[])
 
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-
 	}
+	while (r > 0);
 
 	free(buffer);
 	close_file(from);
